85803f87326e1194db4085864830ccc27243791b
Pedro-Ol1veira <pedru.oliveira865@gmail.com>
2026-01-26 13:00:07 -0300

    Move: Move a pasta /env para dentro da pasta /src

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,6 +1,6 @@
 import "dotenv/config";
 import { defineConfig } from "prisma/config";
-import { env } from './env/index'
+import { env } from './src/env/index'
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
