0b8a586fc88cd9b72f6d75a6e97ba2eb364a759f
Pedro-Ol1veira <pedru.oliveira865@gmail.com>
2026-01-27 11:17:48 -0300

    Test: Adiciona testes para tentativa de autenticação com email e senha errado

--- src/useCases/org/authenticate.spec.ts
+++ src/useCases/org/authenticate.spec.ts
@@ -18,16 +18,48 @@ describe("Authenticate ORG Use Case", () => {
       name: "Minha org",
       phone: "71999999999",
       email: "teste@example.com",
-      password: await hash('123456', 6),
+      password: await hash("123456", 6),
     });
 
     const { org } = await sut.execute({
-        email: "teste@example.com",
-        password: '123456'
+      email: "teste@example.com",
+      password: "123456",
     });
 
     expect(org.id).toEqual(expect.any(String));
   });
 
-  
+  it("Should not authenticate an org with wrong email", async () => {
+    await orgsRepository.create({
+      address: "Salvador",
+      name: "Minha org",
+      phone: "71999999999",
+      email: "teste@example.com",
+      password: await hash("123456", 6),
+    });
+
+    await expect(
+      sut.execute({
+        email: "teste3@example.com",
+        password: "123456",
+      }),
+    ).rejects.toBeInstanceOf(Error);
+  });
+
+  it("Should not authenticate an org with wrong password", async () => {
+    await orgsRepository.create({
+      address: "Salvador",
+      name: "Minha org",
+      phone: "71999999999",
+      email: "teste@example.com",
+      password: await hash("123456", 6),
+    });
+
+    await expect(
+      sut.execute({
+        email: "teste@example.com",
+        password: "1234563",
+      }),
+    ).rejects.toBeInstanceOf(Error);
+  });
 });
